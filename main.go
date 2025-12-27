6063d2929bc3b7ae4ac999d574ac446989aec61b
ChelsYP <chelsayogapermadany2005@mail.ugm.ac.id>
2025-12-27 20:36:35 +0700

    Quiz 2

--- main.go
+++ main.go
@@ -2,15 +2,82 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ChelsYP/belajar-golang/calculation"
+	// "github.com/ChelsYP/belajar-golang/calculation"
 )
 
 func main(){
-	result := calculation.Kali(99, 99)
-	fmt.Println(result)
+	
+	// for i := 1; i <= 100; i++ {
+	// 	fmt.Println("Saya bertekad jago ngoding", i)
+	// }
+
+	judul := "teknologi rekayasa internet"
+
+	for index, letter := range judul {
+		if index % 2 == 0 {
+			fmt.Println(index, "Letter: ", string(letter))
+		}
+	}
+
+	for index, letter := range judul {
+		letterString := string(letter)
+
+		if letterString == "a" ||letterString == "i" ||letterString == "u"||letterString == "e"||letterString == "o"{
+			fmt.Println(index, string(letter))
+		}		
+	}
+	// switch angka {
+	// case 1:
+	// 	fmt.Println("Satu")
+	// case 2:
+	// 	fmt.Println("Dua")
+	// case 3:
+	// 	fmt.Println("Tiga")
+	// default:
+	// 	fmt.Println("Standard")
+	// }
+
+	// result := calculation.Kali(99, 99)
+	// fmt.Println(result)
+	// name := "Chelsa Yoga"
+	// angka := 69
+	// laki := "true"
+	// tinggi := 178.9
+
+	// fmt.Println(name)	
+	// fmt.Println(angka)
+	// fmt.Println(laki)
+	// fmt.Println(tinggi)
+	
+	// umur := 90
+	// if umur > 90 {
+	// 	fmt.Println("Boleh bermain")
+	// } else {
+	// 	fmt.Println("Belum boleh dek")
+	// }
+
+	// 	nilai := 60
+	// 	var tier string
+
+	// 	if nilai <=50 {
+	// 		tier = "E"
+	// 	} else if nilai <=60 {
+	// 		tier = "D"
+	// 	} else if nilai <70 {
+	// 		tier = "C"
+	// 	} else {
+	// 		tier ="Anomali"
+	// 	}
+
+	// 	fmt.Println(tier)
 }
+
 // FUNGSI import
 // 1. Standard Library
 // 2. Paket yang berbeda
 // 3. Mengakses kode atau library yang dibikin orang lain 
+
+// Mendefinisikan Tipe data
+// Rules => "Variabel := ""
+
+// if, if else, else, if, if bersarang
