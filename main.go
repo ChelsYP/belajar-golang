c205f966d5d5564e7722f5617f4e531e2ea3f9a4
ChelsYP <chelsayogapermadany2005@mail.ugm.ac.id>
2025-12-28 23:02:41 +0700

    mapbasic

--- main.go
+++ main.go
@@ -3,25 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	// gamingConsole := []string{"PS5", "PC", "HP"}
-	var gamingConsole []string
+	var myMap map[string]int
+	myMap = map[string]int{}
 
-	gamingConsole = append(gamingConsole, "PeS2025")
-	gamingConsole = append(gamingConsole, "PeS2026")
-	gamingConsole = append(gamingConsole, "PeS2027")
-	
-	for _, console := range gamingConsole {
-		fmt.Println(console)
-	}
-	
+	myMap["Solana"] = 9
+	myMap["Soldiity"] = 9
+	myMap["Base"] = 10
+
+	fmt.Println(myMap) 
 }
-
-
-
-
-
-
-
 
 
 
@@ -38,13 +28,21 @@ func main(){
 // import "fmt"
 
 // func main(){
-	
-// 	languages := [...]string{"Go", "Rush", "Solana", "Solidity"}
+// 	// gamingConsole := []string{"PS5", "PC", "HP"}
+// 	var gamingConsole []string
 
-// 	for index, lang := range languages {
-// 		fmt.Println("Index : ", index, "language : ", lang)
+// 	gamingConsole = append(gamingConsole, "PeS2025")
+// 	gamingConsole = append(gamingConsole, "PeS2026")
+// 	gamingConsole = append(gamingConsole, "PeS2027")
+	
+// 	for _, console := range gamingConsole {
+// 		fmt.Println(console)
 // 	}
+	
 // }
+
+
+
 
 
 
@@ -62,84 +60,111 @@ func main(){
 
 // // package main
 
-// // import (
-// // 	"fmt"
-// // 	// "github.com/ChelsYP/belajar-golang/calculation"
-// // )
+// // import "fmt"
 
 // // func main(){
 	
-// // 	// for i := 1; i <= 100; i++ {
-// // 	// 	fmt.Println("Saya bertekad jago ngoding", i)
-// // 	// }
+// // 	languages := [...]string{"Go", "Rush", "Solana", "Solidity"}
 
-// // 	judul := "teknologi rekayasa internet"
-
-// // 	for index, letter := range judul {
-// // 		if index % 2 == 0 {
-// // 			fmt.Println(index, "Letter: ", string(letter))
-// // 		}
+// // 	for index, lang := range languages {
+// // 		fmt.Println("Index : ", index, "language : ", lang)
 // // 	}
-
-// // 	for index, letter := range judul {
-// // 		letterString := string(letter)
-
-// // 		if letterString == "a" ||letterString == "i" ||letterString == "u"||letterString == "e"||letterString == "o"{
-// // 			fmt.Println(index, string(letter))
-// // 		}		
-// // 	}
-// // 	// switch angka {
-// // 	// case 1:
-// // 	// 	fmt.Println("Satu")
-// // 	// case 2:
-// // 	// 	fmt.Println("Dua")
-// // 	// case 3:
-// // 	// 	fmt.Println("Tiga")
-// // 	// default:
-// // 	// 	fmt.Println("Standard")
-// // 	// }
-
-// // 	// result := calculation.Kali(99, 99)
-// // 	// fmt.Println(result)
-// // 	// name := "Chelsa Yoga"
-// // 	// angka := 69
-// // 	// laki := "true"
-// // 	// tinggi := 178.9
-
-// // 	// fmt.Println(name)	
-// // 	// fmt.Println(angka)
-// // 	// fmt.Println(laki)
-// // 	// fmt.Println(tinggi)
-	
-// // 	// umur := 90
-// // 	// if umur > 90 {
-// // 	// 	fmt.Println("Boleh bermain")
-// // 	// } else {
-// // 	// 	fmt.Println("Belum boleh dek")
-// // 	// }
-
-// // 	// 	nilai := 60
-// // 	// 	var tier string
-
-// // 	// 	if nilai <=50 {
-// // 	// 		tier = "E"
-// // 	// 	} else if nilai <=60 {
-// // 	// 		tier = "D"
-// // 	// 	} else if nilai <70 {
-// // 	// 		tier = "C"
-// // 	// 	} else {
-// // 	// 		tier ="Anomali"
-// // 	// 	}
-
-// // 	// 	fmt.Println(tier)
 // // }
 
-// // // FUNGSI import
-// // // 1. Standard Library
-// // // 2. Paket yang berbeda
-// // // 3. Mengakses kode atau library yang dibikin orang lain 
 
-// // // Mendefinisikan Tipe data
-// // // Rules => "Variabel := ""
 
-// // // if, if else, else, if, if bersarang
+
+
+
+
+
+
+
+
+
+
+
+
+// // // package main
+
+// // // import (
+// // // 	"fmt"
+// // // 	// "github.com/ChelsYP/belajar-golang/calculation"
+// // // )
+
+// // // func main(){
+	
+// // // 	// for i := 1; i <= 100; i++ {
+// // // 	// 	fmt.Println("Saya bertekad jago ngoding", i)
+// // // 	// }
+
+// // // 	judul := "teknologi rekayasa internet"
+
+// // // 	for index, letter := range judul {
+// // // 		if index % 2 == 0 {
+// // // 			fmt.Println(index, "Letter: ", string(letter))
+// // // 		}
+// // // 	}
+
+// // // 	for index, letter := range judul {
+// // // 		letterString := string(letter)
+
+// // // 		if letterString == "a" ||letterString == "i" ||letterString == "u"||letterString == "e"||letterString == "o"{
+// // // 			fmt.Println(index, string(letter))
+// // // 		}		
+// // // 	}
+// // // 	// switch angka {
+// // // 	// case 1:
+// // // 	// 	fmt.Println("Satu")
+// // // 	// case 2:
+// // // 	// 	fmt.Println("Dua")
+// // // 	// case 3:
+// // // 	// 	fmt.Println("Tiga")
+// // // 	// default:
+// // // 	// 	fmt.Println("Standard")
+// // // 	// }
+
+// // // 	// result := calculation.Kali(99, 99)
+// // // 	// fmt.Println(result)
+// // // 	// name := "Chelsa Yoga"
+// // // 	// angka := 69
+// // // 	// laki := "true"
+// // // 	// tinggi := 178.9
+
+// // // 	// fmt.Println(name)	
+// // // 	// fmt.Println(angka)
+// // // 	// fmt.Println(laki)
+// // // 	// fmt.Println(tinggi)
+	
+// // // 	// umur := 90
+// // // 	// if umur > 90 {
+// // // 	// 	fmt.Println("Boleh bermain")
+// // // 	// } else {
+// // // 	// 	fmt.Println("Belum boleh dek")
+// // // 	// }
+
+// // // 	// 	nilai := 60
+// // // 	// 	var tier string
+
+// // // 	// 	if nilai <=50 {
+// // // 	// 		tier = "E"
+// // // 	// 	} else if nilai <=60 {
+// // // 	// 		tier = "D"
+// // // 	// 	} else if nilai <70 {
+// // // 	// 		tier = "C"
+// // // 	// 	} else {
+// // // 	// 		tier ="Anomali"
+// // // 	// 	}
+
+// // // 	// 	fmt.Println(tier)
+// // // }
+
+// // // // FUNGSI import
+// // // // 1. Standard Library
+// // // // 2. Paket yang berbeda
+// // // // 3. Mengakses kode atau library yang dibikin orang lain 
+
+// // // // Mendefinisikan Tipe data
+// // // // Rules => "Variabel := ""
+
+// // // // if, if else, else, if, if bersarang
